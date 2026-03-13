2fc63497f796b3141c309caec08b0be625959db0
Rudnasoukadanicia <rsouka@byupathway.edu>
2026-03-13 17:18:46 +0100

    rs--individual

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -2,8 +2,6 @@
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
 }
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
@@ -20,4 +18,23 @@ export function setClick(selector, callback) {
     callback();
   });
   qs(selector).addEventListener("click", callback);
+}
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get(param);
+}
+
+// src/js/utils.mjs
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = 'afterbegin',
+  clear = false
+) {
+  if (!parentElement) return;
+  if (clear) parentElement.innerHTML = '';
+  const html = list.map(templateFn).join('');
+  parentElement.insertAdjacentHTML(position, html);
 }
